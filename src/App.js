104e92a66dbcfbd733cd30e0a3410a98bb1e01eb
viktor <vikhulei@gmail.com>
2021-06-27 13:58:02 +0300

    Counter with state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import React, {useState, useEffect} from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+
+const [counter, setCounter] = useState(0)
+
+const increment = () => {
+  setCounter(counter + 1)
+}
+
+const decrement = () => {
+  setCounter(counter - 1)
+}
+
+return (
+  <div className="App">
+    <button onClick={(() => setCounter(counter+1))}>+</button>
+    <h1>{counter}</h1>
+    <button onClick={(() => setCounter(counter - 1))}>-</button>
+  </div>
+);
 }
 
 export default App;
