19e451c35b236de2be7ee588ab536b42953d7887
viktor <vikhulei@gmail.com>
2021-06-27 14:00:02 +0300

    Blank branch created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import './App.css';
-import React, {useState, useEffect} from "react"
 
 function App() {
 
-const [counter, setCounter] = useState(0)
-
-const increment = () => {
-  setCounter(counter + 1)
-}
-
-const decrement = () => {
-  setCounter(counter - 1)
-}
 
 return (
   <div className="App">
-    <button onClick={(() => setCounter(counter+1))}>+</button>
-    <h1>{counter}</h1>
-    <button onClick={(() => setCounter(counter - 1))}>-</button>
+    <h1>Hi</h1>
   </div>
 );
 }
